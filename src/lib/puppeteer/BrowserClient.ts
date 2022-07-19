d2352223d91c54b327549bdf6ad0cd48457fd30f
the-engineer4 <downer.ryan@gmail.com>
2022-07-18 19:12:30 -0500

    1 file added, 1 deleted

--- src/lib/puppeteer/BrowserClient.ts
+++ src/lib/puppeteer/BrowserClient.ts
@@ -1,9 +1,8 @@
 import {Browser} from 'puppeteer';
-import type * from 'puppeteer';
 
 export class BrowserClient {
   public:
-    init = async (): Promise<Browser> => {
+    const init = async (): Promise<Browser> => {
 
     }
 
